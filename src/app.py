6ba1ab5c76e8a494d56d3b7d6d9a0881824c8831
MohamedaliBz <Mohamedalibaazaoui521@gmail.com>
2026-04-30 07:05:10 +0100

    first commit

---
+++ src/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, request
+import sqlite3
+import os
+
+app = Flask(__name__)
+
+# ❌ Secret hardcodé
+API_KEY = "AKIA_TEST_SECRET_123"
+
+def get_db():
+    return sqlite3.connect("users.db")
+
+@app.route("/")
+def home():
+    return "DevSecOps Demo"
+
+@app.route("/search")
+def search():
+    name = request.args.get("name", "")
+    
+    # ❌ SQL Injection
+    query = f"SELECT * FROM users WHERE name = '{name}'"
+    
+    conn = get_db()
+    result = conn.execute(query).fetchall()
+    
+    return str(result)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
